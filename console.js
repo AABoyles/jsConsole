6351a66256e68016ca0613a5867d5e08492910f9
Tony Boyles <tboyles@sosacorp.com>
2015-03-09 17:08:43 -0400

    Added some nice menubars for all the nifty, editor specific features I mean to build out.

--- console.js
+++ console.js
@@ -1,24 +1,36 @@
 var input = "{}", output = "{}", report = function(error){};
 
 $(function(){
-  inputeditor = ace.edit("origdata");
-  inputeditor.setTheme("ace/theme/github");
-  inputeditor.getSession().setMode("ace/mode/json");
-
   jseditor = ace.edit("script");
   jseditor.setTheme("ace/theme/github");
   jseditor.getSession().setMode("ace/mode/javascript");
+
+  inputeditor = ace.edit("origdata");
+  inputeditor.setTheme("ace/theme/github");
+  inputeditor.getSession().setMode("ace/mode/json");
 
   outeditor = ace.edit("outdata");
   outeditor.setTheme("ace/theme/github");
   outeditor.getSession().setMode("ace/mode/json");
   outeditor.setReadOnly(true);
 
-  $("#execute").click(function(){
+  var translate = function(){
     input = JSON.parse(inputeditor.getValue());
     eval(jseditor.getValue());
-    outeditor.setValue(JSON.stringify(output));
+    var pretty = $('input[name="prints"]:checked').attr("id") == "pretty" ? 2 : 0;
+    outeditor.setValue(JSON.stringify(output, null, pretty));
     $('#output > ul > li > a:last').tab('show');
-  });
-});
+  };
 
+  $("#execute").click(translate);
+  $('input[name="prints"]').change(translate);
+
+  $("#newscript").click(function(){
+    jseditor.setValue("output = _(input);");
+  });
+
+  $("#newinput").click(function(){
+    inputeditor.setValue("{}")
+  });
+
+});
