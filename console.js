f5f058a27ace27bdc6175e61b9eded3660a96869
Tony Boyles <tboyles@sosacorp.com>
2015-03-10 13:40:02 -0400

    All the buttons now do relatively intuitive things.

--- console.js
+++ console.js
@@ -22,15 +22,79 @@ $(function(){
     $('#output > ul > li > a:last').tab('show');
   };
 
+  $("#loadscript").click(function(){
+    if($(this).hasClass("active")){
+      $("#script-file").parent().fadeOut();
+    } else {
+      $("#script-file").parent().fadeIn();
+    }
+  });
+
+  $("#script-file").change(function(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    var file = evt.target.files[0];
+    var reader = new FileReader();
+    reader.onloadend = function(evt) {
+      if (evt.target.readyState == FileReader.DONE) {
+        jseditor.setValue(evt.target.result);
+        $("#script-file").parent().fadeOut();
+        $("#loadscript").removeClass("active");
+      }
+    }
+    reader.readAsText(file);
+  });
+
+  $("#savescript").mousedown(function(){
+    this.href="data:text;charset=utf8,"+jseditor.getValue();
+    this.download="edit.js";
+  });
+
   $("#execute").click(translate);
-  $('input[name="prints"]').change(translate);
+
+  $("#loadinput").click(function(){
+    if($(this).hasClass("active")){
+      $("#origdata-file").parent().fadeOut();
+    } else {
+      $("#origdata-file").parent().fadeIn();
+    }
+  });
+
+  $("#origdata-file").change(function(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    var file = evt.target.files[0];
+    var reader = new FileReader();
+    reader.onloadend = function(evt) {
+      if (evt.target.readyState == FileReader.DONE) {
+        inputeditor.setValue(evt.target.result);
+        $("#origdata-file").parent().fadeOut();
+        $("#loadinput").removeClass("active");
+      }
+    }
+    reader.readAsText(file);
+  });
 
   $("#newscript").click(function(){
     jseditor.setValue("output = _(input);");
+    $("#origdata-file")[0].value = ""
   });
 
   $("#newinput").click(function(){
-    inputeditor.setValue("{}")
+    inputeditor.setValue("{}");
   });
 
+  $("#saveinput").mousedown(function(){
+    this.href="data:text;charset=utf8,"+inputeditor.getValue();
+    this.download="input.json";
+  });
+
+  $("#saveoutput").mousedown(function(){
+    this.href="data:text;charset=utf8,"+outeditor.getValue();
+    this.download="output.json";
+  });
+
+  $('input[name="prints"]').change(translate);
+
 });
+
